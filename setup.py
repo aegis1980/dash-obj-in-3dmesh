535e502c6728c9c628375f483ade400223c6ee7c
Jon Robinson <jonrobinson@hotmail.com>
2019-11-07 11:17:24 +1300

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ from setuptools import setup
 
 from os import path
 
+here = path.abspath(path.dirname(__file__))
+
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
