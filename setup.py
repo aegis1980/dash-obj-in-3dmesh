172867ee97c91288654e265ced3f7c399f4a5887
Jon Robinson <jonrobinson@hotmail.com>
2019-11-26 11:38:14 +1300

    Edits to setup.py

--- setup.py
+++ setup.py
@@ -4,29 +4,42 @@ https://packaging.python.org/guides/distributing-packages-using-setuptools/
 https://github.com/pypa/sampleproject
 """
 
-from setuptools import setup
+from setuptools import find_packages,setup
 from os import path
 
-here = path.abspath(path.dirname(__file__))
+HERE = path.dirname(path.abspath(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+GITHUB_URL = "https://github.com/aegis1980/dash-obj-in-3dmesh/"
+
+def _get_long_description():
+    """
+    Get the long description from the README file
+    """
+    with open(path.join(HERE, "README.md")) as f:
+        return f.read()
+
 
 setup(
-    name='dash_obj_in_3dmesh',
-    version='0.1.dev',
-    description='Some tools for getting Wavefront OBJ files into a dash 3dmesh graph',
-    long_description=open('README.md').read(),
+    name='dash-obj-in-3dmesh',
+    version='0.1-dev',
+    description='Some tools for getting Wavefront OBJ files into a Plotly Dash 3dmesh graph',
+    long_description=_get_long_description(),
     long_description_content_type='text/markdown', 
     author='Jon Robinson',
     author_email='jonrobinson1980@gmail.com',
-    package_dir={'dash_obj_in_3dmesh': 'dash_obj_in_3dmesh'},
-    packages=['dash_obj_in_3dmesh'], 
+    license='CC Attribution 4.0 International',
+    packages=find_packages(),
     python_requires='>=3.5',
     install_requires=[
         'numpy', 
         'dash',
         'multimethod'
     ], #external packages as dependencies
+    classifiers=[
+        "Framework :: Dash",
+        "License :: OSI Approved :: CC Attribution 4.0 International",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ]
 )
